1488eae4b14a91e0ac2a25bc2e9cf746ed0a7d50
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2018-09-28 10:59:52 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -630,7 +630,7 @@ $(window).on('load', function() {
     addBaseMap();
 
     // Add point markers to the map
-    /*var points = pointData.sheets(constants.pointsSheetName);
+    var points = pointData.sheets(constants.pointsSheetName);
     var layers;
     var group = '';
     if (points && points.elements.length > 0) {
@@ -640,7 +640,7 @@ $(window).on('load', function() {
       completePoints = true;
     }
 
-    centerAndZoomMap(group);*/
+    centerAndZoomMap(group);
 
     // Add polylines
     var polylines = mapData.sheets(constants.polylinesSheetName);
