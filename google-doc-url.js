773e2bf5f1b3f1151c1ecf3108c806c54c84ab92
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2018-09-28 10:04:47 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pILcAk-msN0pLJmQ4puFFHD7WL5f8ZzmGYJI1kFbKPk/edit#gid=0';
-var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1ID05WlFWD7y5zPCJ8godU8LlzhR1SI07kE-7inWTpbo/edit#gid=0';
+var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1jN0e-1Lg-_Vd_pUO7oXVx47PLjmxY9SxXV_SzMEONK8/edit#gid=0';
