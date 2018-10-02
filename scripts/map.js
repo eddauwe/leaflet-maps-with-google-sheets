5d70214865468f3631f46ff6f370f14ee7a333b4
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2018-10-02 10:45:08 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -630,7 +630,7 @@ $(window).on('load', function() {
     addBaseMap();
 
     // Add point markers to the map
-    /*var points = pointData.sheets(constants.pointsSheetName);
+    var points = pointData.sheets(constants.pointsSheetName);
     var layers;
     var group = '';
     if (points && points.elements.length > 0) {
@@ -640,7 +640,7 @@ $(window).on('load', function() {
       completePoints = true;
     }
 
-    centerAndZoomMap(group);*/
+    centerAndZoomMap(group);
 
     // Add polylines
     var polylines = mapData.sheets(constants.polylinesSheetName);
@@ -1037,7 +1037,7 @@ $(window).on('load', function() {
          // the Google sheet
          pointData = Tabletop.init({
            key: googleDocURLpoints,
-           callback: function(data, pointData) { onPointDataLoad(); }
+           callback: function(data, pointData) { onMapDataLoad(); }
          });
        },
        success: function() {
@@ -1046,7 +1046,7 @@ $(window).on('load', function() {
          pointData.load({
            self: pointData,
            tabs: ['Points'],
-           callback: onPointDataLoad
+           callback: onMapDataLoad
          });
        }
    });
