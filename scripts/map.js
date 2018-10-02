add2c9681f498993c7b4a5f967f60464a570bf64
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2018-10-02 16:16:58 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -614,7 +614,7 @@ $(window).on('load', function() {
     } else {
       completePoints = true;
     }
-    //centerAndZoomMap(group);
+    centerAndZoomMap(group);
   }
   /**
    * Here all data processing from the spreadsheet happens
