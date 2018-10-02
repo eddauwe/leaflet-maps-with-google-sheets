626c0087f8f3e94b1ef471bbc904655367f82295
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2018-10-02 10:52:24 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -605,11 +605,11 @@ $(window).on('load', function() {
     var group = '';
     if (points && points.elements.length > 0) {
       layers = determineLayers(points.elements);
-      group = mapPoints(points.elements, layers);
+      //group = mapPoints(points.elements, layers);
     } else {
       completePoints = true;
     }
-    centerAndZoomMap(group);
+    //centerAndZoomMap(group);
   }
   /**
    * Here all data processing from the spreadsheet happens
@@ -630,7 +630,7 @@ $(window).on('load', function() {
     addBaseMap();
 
     // Add point markers to the map
-    var points = pointData.sheets(constants.pointsSheetName);
+    /*var points = pointData.sheets(constants.pointsSheetName);
     var layers;
     var group = '';
     if (points && points.elements.length > 0) {
@@ -640,7 +640,7 @@ $(window).on('load', function() {
       completePoints = true;
     }
 
-    centerAndZoomMap(group);
+    centerAndZoomMap(group);*/
 
     // Add polylines
     var polylines = mapData.sheets(constants.polylinesSheetName);
@@ -1037,7 +1037,7 @@ $(window).on('load', function() {
          // the Google sheet
          pointData = Tabletop.init({
            key: googleDocURLpoints,
-           callback: function(data, pointData) { onMapDataLoad(); }
+           callback: function(data, pointData) { onPointDataLoad(); }
          });
        },
        success: function() {
@@ -1046,7 +1046,7 @@ $(window).on('load', function() {
          pointData.load({
            self: pointData,
            tabs: ['Points'],
-           callback: onMapDataLoad
+           callback: onPointDataLoad
          });
        }
    });
