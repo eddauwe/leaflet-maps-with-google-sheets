23ffd233bd608f956f5eb37e31bb915abd95f79d
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2018-10-04 14:42:08 +0200

    Update maproute.js

--- scripts/maproute.js
+++ scripts/maproute.js
@@ -140,12 +140,12 @@ $(window).on('load', function() {
 
         markerArray.push(marker);
       }
-      if (points.Route !== '')
-      {var gpx = 'gesamtstrecke-eifelsteig.gpx'; // URL to your GPX file or the GPX itself
+      
+      var gpx = 'gesamtstrecke-eifelsteig.gpx'; // URL to your GPX file or the GPX itself
 new L.GPX(gpx, {async: true}).on('loaded', function(e) {
   map.fitBounds(e.target.getBounds());
 }).addTo(map);
-      }
+      
     }
 
     var group = L.featureGroup(markerArray);
