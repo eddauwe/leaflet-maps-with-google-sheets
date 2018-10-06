ddc2649b1927f41077703c32581f30e40a0840c7
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2018-10-06 17:41:43 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -585,11 +585,11 @@ $(window).on('load', function() {
     var group = '';
     if (points && points.elements.length > 0) {
       layers = determineLayers(points.elements,icons.elements);
-      //group = mapPoints(points.elements,icons.elements,layers);
+      group = mapPoints(points.elements,icons.elements,layers);
     } else {
       completePoints = true;
     }
-    //centerAndZoomMap(group);
+    centerAndZoomMap(group);
   }
   /**
    * Here all data processing from the spreadsheet happens
