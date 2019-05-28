bd2f3d356fa17278dab422de8727f1cabf87f70d
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2019-05-28 14:57:22 +0200

    back to old way of showing legend

--- scripts/map.js
+++ scripts/map.js
@@ -757,10 +757,10 @@ $(window).on('load', function() {
         });
         
         
+        $('.ladder h6').get(0).click();
+
         $('#map').css('visibility', 'visible');
         $('.loader').hide();
-
-        $('.ladder h6').click();
         
         // Open intro popup window in the center of the map
         if (getSetting('_introPopupText') != '') {
