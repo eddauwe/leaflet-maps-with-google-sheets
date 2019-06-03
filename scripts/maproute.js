bb252f4976401770de96affb43096a271b382d0c
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2019-06-03 19:30:16 +0200

    Update maproute.js

--- scripts/maproute.js
+++ scripts/maproute.js
@@ -169,11 +169,6 @@ new L.GPX(gpx, {async: true,polyline_options: {
       {var gpx = point['Location']; //line['Location'] URL to your GPX file or the GPX itself
        if (layers !== undefined && layers.length !== 0) {
          route=new L.GPX(gpx, {async: true,
-                               marker_options: {
-    startIconUrl: 'images/pin-icon-start.png',
-    endIconUrl: 'images/pin-icon-end.png',                                 
-    shadowUrl: 'images/pin-shadow.png'
-  }
                               }).on('loaded', function(e) {
   map.fitBounds(e.target.getBounds());
 });
