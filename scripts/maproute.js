1265dcee5044ae7f1da1e64d9f8212725bde321e
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2019-06-04 09:28:52 +0200

    don't zoom to route layer

--- scripts/maproute.js
+++ scripts/maproute.js
@@ -169,9 +169,7 @@ new L.GPX(gpx, {async: true,polyline_options: {
       {var gpx = point['Location']; //line['Location'] URL to your GPX file or the GPX itself
        if (layers !== undefined && layers.length !== 0) {
          route=new L.GPX(gpx, {async: true,
-                              }).on('loaded', function(e) {
-  map.fitBounds(e.target.getBounds());
-});
+                              })
          route.addTo(layers[point.Group]);
        }
       }
