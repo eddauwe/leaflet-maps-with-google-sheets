4585ff56e664f0c42d6c4a2415968d9f3178d1a3
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2019-06-04 09:31:47 +0200

    minor

--- scripts/maproute.js
+++ scripts/maproute.js
@@ -169,7 +169,7 @@ new L.GPX(gpx, {async: true,polyline_options: {
       {var gpx = point['Location']; //line['Location'] URL to your GPX file or the GPX itself
        if (layers !== undefined && layers.length !== 0) {
          route=new L.GPX(gpx, {async: true,
-                              })
+                              });
          route.addTo(layers[point.Group]);
        }
       }
