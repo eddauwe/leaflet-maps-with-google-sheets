ca8104a15044b61c249f512d1b34e8980fb9739f
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2019-07-05 10:54:51 +0200

    trouwfeest locaties

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,4 +1,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pILcAk-msN0pLJmQ4puFFHD7WL5f8ZzmGYJI1kFbKPk/edit#gid=0';
 //var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1ID05WlFWD7y5zPCJ8godU8LlzhR1SI07kE-7inWTpbo/edit#gid=0';
-var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1ksP4-uRPUSbCDW-7VbVei7xKp3ddv8IJjsp_M5z4Wno/edit#gid=164271551';
+var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/10j2RUdLMH_80buWq8NIcHQ0cx_gHCnfVBspBc0x0Wyo/edit#gid=164271551';
 
