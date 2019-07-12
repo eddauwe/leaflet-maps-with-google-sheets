59811e105607cf2d488cba243eb54ffbee68edfa
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2019-07-12 09:54:19 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -3,3 +3,6 @@ var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pILcAk-msN0pLJmQ4puF
 var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/10j2RUdLMH_80buWq8NIcHQ0cx_gHCnfVBspBc0x0Wyo/edit#gid=164271551';
 var googleDocURLreceptie='https://docs.google.com/spreadsheets/d/1R_5tZo6OigFcFBP3TPxpRj0heqi2740Hn543vgPZFIQ/edit#gid=1619475893';
 
+//moezelreis
+var googleDocURLmoezel='https://docs.google.com/spreadsheets/d/1jN0e-1Lg-_Vd_pUO7oXVx47PLjmxY9SxXV_SzMEONK8/edit#gid=164271551';
+
