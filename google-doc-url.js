4bed20212a2bc257754dbe3d3a8ba21250006fc3
EDWAZ56 <Eduard.Dauwe@realdolmen.com>
2020-02-18 16:22:27 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,4 +1,4 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pILcAk-msN0pLJmQ4puFFHD7WL5f8ZzmGYJI1kFbKPk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KyNe7pQSL6iKqYg2sY6EHVV5wcNijsbUC22S7X7jOpA/edit#gid=472629121';
 //var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1ID05WlFWD7y5zPCJ8godU8LlzhR1SI07kE-7inWTpbo/edit#gid=0';
 var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/10j2RUdLMH_80buWq8NIcHQ0cx_gHCnfVBspBc0x0Wyo/edit#gid=164271551';
 var googleDocURLreceptie='https://docs.google.com/spreadsheets/d/1R_5tZo6OigFcFBP3TPxpRj0heqi2740Hn543vgPZFIQ/edit#gid=1619475893';
