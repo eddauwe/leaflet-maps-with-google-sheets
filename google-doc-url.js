2ccdabd115be47112f8f0334efbf413b594908f8
eddauwe <98811987+eddauwe@users.noreply.github.com>
2022-02-01 19:06:01 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,6 +1,6 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mOBUbBVXFkRgtACWeZU8R3Q0OiOtaKw_SeUhtNbbcF4/edit#gid=164271551';
 //var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1ID05WlFWD7y5zPCJ8godU8LlzhR1SI07kE-7inWTpbo/edit#gid=0';
-var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1jN0e-1Lg-_Vd_pUO7oXVx47PLjmxY9SxXV_SzMEONK8/edit#gid=164271551';
+var googleDocURLpoints = 'https://docs.google.com/spreadsheets/d/1mOBUbBVXFkRgtACWeZU8R3Q0OiOtaKw_SeUhtNbbcF4/edit#gid=164271551';
 var googleDocURLreceptie='https://docs.google.com/spreadsheets/d/1R_5tZo6OigFcFBP3TPxpRj0heqi2740Hn543vgPZFIQ/edit#gid=1619475893';
 
 //moezelreis
