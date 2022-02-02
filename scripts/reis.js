d4b4b8560bb17c7de912c9f96d78538c7be5e3fc
eddauwe <98811987+eddauwe@users.noreply.github.com>
2022-02-02 20:40:09 +0100

    voeg twee accolades toe

--- scripts/reis.js
+++ scripts/reis.js
@@ -126,7 +126,7 @@ $(window).on('load', function() {
           iconel['Marker Color'].toLowerCase(),
           iconel['Icon Color']
         );
-
+	  }}
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
