782e2048beff3db5df120f97f31194f01e11d95e
eddauwe <98811987+eddauwe@users.noreply.github.com>
2025-04-29 12:35:10 +0200

    maak osm default map now oa basemap is removed

--- scripts/reis.js
+++ scripts/reis.js
@@ -1031,6 +1031,7 @@ $(window).on('load', function() {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
+    OpenStreetMap_Mapnik.addTo(map);
    
     var CyclOSM = L.tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
 	maxZoom: 20,
