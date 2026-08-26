1b8e4d1f75f5e45ea6954b9cf870b323dbf5afe0
eddauwe <98811987+eddauwe@users.noreply.github.com>
2026-08-26 14:33:10 +0200

    Set opacity of weather map layers to 1
    Updated opacity for various weather map layers to 1.

--- scripts/reis.js
+++ scripts/reis.js
@@ -1066,31 +1066,31 @@ precipitation:
 L.tileLayer(`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.
 png?appid=${API_KEY}`, {
 attribution: '© OpenWeatherMap',
-opacity: 0.9
+opacity: 1
 }),
 clouds:
 L.tileLayer(`https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?a
 ppid=${API_KEY}`, {
 attribution: '© OpenWeatherMap',
-opacity: 0.9
+opacity: 1
 }),
 pressure:
 L.tileLayer(`https://tile.openweathermap.org/map/pressure_new/{z}/{x}/{y}.png?
 appid=${API_KEY}`, {
 attribution: '© OpenWeatherMap',
-	opacity: 0.9
+	opacity: 1
 }),
 wind:
 L.tileLayer(`https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?app
 id=${API_KEY}`, {
 attribution: '© OpenWeatherMap',
-opacity: 0.9
+opacity: 1
 }),
 temperature:
 L.tileLayer(`https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?app
 id=${API_KEY}`, {
 attribution: '© OpenWeatherMap',
-opacity: 0.9
+opacity: 1
 })
 };
 
