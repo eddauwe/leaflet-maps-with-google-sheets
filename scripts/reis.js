f8cc2bb1c1b13ff067d749dfebc3fd86e17eaf38
eddauwe <98811987+eddauwe@users.noreply.github.com>
2026-08-26 11:07:02 +0200

    Add WMS layer options for maxcc, zoom and time

--- scripts/reis.js
+++ scripts/reis.js
@@ -1063,7 +1063,11 @@ var Thunderforest_OpenCycleMap = L.tileLayer('https://tile.thunderforest.com/cyc
  const layer = L.tileLayer.wms(baseUrl, {
  tileSize: 512,
  attribution: '&copy; <a href="https://dataspace.copernicus.eu/" target="_blank">Copernicus Data Space Ecosystem</a>',
- layers: layerId
+ maxcc: 20,
+ minZoom: 6,
+ maxZoom: 16,
+ layers: layerId,
+ time:"2023-06-01/2026-06-01"
  });
  return layer;
  }
